045a5a1c93b747e44d88359760472664e2f1e93d
Jean <panezrimacjeanpaul@gmail.com>
2023-12-23 18:11:22 -0500

    ArticTec

--- js/envio.js
+++ js/envio.js
@@ -4,7 +4,7 @@ const enviarFormulario =() => {
         let dimension = document.getElementById('dimension').value;
         let personaje = document.getElementById('personaje').value;
         let mensaje = document.getElementById('mensaje').value;
-        let numero= +51926598644;
+        let numero= +51;
 var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}%0ALos%20datos%20de%20mi%20compra%20son%20los%20siguientes:
 %0A•%20AEl%20tamaño%20de%20mi%20producto%20es:%20${dimension}%0A•%20El%20personaje%20o%20asunto
 %20es:%20${personaje}%0A•%20Algunas%20características%20adicionales%20de%20mi%20producto:%20${mensaje}`,'_blank');       
